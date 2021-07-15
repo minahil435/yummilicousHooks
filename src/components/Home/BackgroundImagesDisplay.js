4132693a2760f56c027f48d357cf08988580c816
Minahil shafique <minahal.shafique@gmail.com>
2021-07-15 19:51:08 -0400

    component to display background

---
+++ src/components/Home/BackgroundImagesDisplay.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+
+export class BackgroundImagesDisplay extends Component {
+
+    onHandleEditSubmit = (index,mode) => {
+        console.log(index)
+        if (mode && (index > 3)){
+            return true
+        }
+        return false
+       
+    };
+
+    render (){
+        return (
+            <div>
+                <img
+                    className="wallpaper"
+                    src = {this.props.item}
+                    alt = {this.props.item}
+                    height = {this.onHandleEditSubmit(this.props.index , this.props.searchModeOn) ? "0" : "230"}
+                    />
+            </div>
+        )
+    }
+}
+
+export default BackgroundImagesDisplay;
